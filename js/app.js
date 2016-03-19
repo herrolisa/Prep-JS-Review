7e9f1823dcabdb8b4c1a6281c6dad1e404aaf0c6
Lisa Zhou <Lisa Zhou>
2016-03-18 20:45:21 -1000

    completed exercise 6

--- js/app.js
+++ js/app.js
@@ -52,6 +52,10 @@ Declare a variable named planets and assign the value of each planet in our sola
 Console log the variable
 Console log "Earth" using the index position
 Console log "Uranus" using the index position*/
+var planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
+console.log(planets);
+console.log(planets[2]);
+console.log(planets[6]);
 
 /*7. Variables with Array for its value.
 Declare a variable named india and assign the value of 3 popular tourist attractions.  
