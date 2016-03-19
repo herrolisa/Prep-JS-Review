131dccdf3a51fefee78834de646c0e2f0219a00f
Lisa Zhou <Lisa Zhou>
2016-03-18 20:40:24 -1000

    completed exercise 5

--- js/app.js
+++ js/app.js
@@ -42,7 +42,10 @@ Declare a variable named japanPrefectures and assign the value of the names of 5
 Console log the variable 
 Console log the value at index 1.
 Console log the value at index 3.*/
-
+var japanPrefectures = ["Hokkaido", "Tokyo", "Chiba", "Kanagawa", "Miyagi"];
+console.log(japanPrefectures);
+console.log(japanPrefectures[1]);
+console.log(japanPrefectures[3]);
 
 /*6.  Variable with Array for its value.
 Declare a variable named planets and assign the value of each planet in our solar system, in the order of distance from the sun.  
